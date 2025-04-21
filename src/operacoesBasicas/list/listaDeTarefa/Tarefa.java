8052e7dcdd8f788b22334dec214050ea18920d77
Rafael Lira <rafaelira1110@gmail.com>
2025-04-21 14:26:39 -0300

    exercícios sobre list

---
+++ src/operacoesBasicas/list/listaDeTarefa/Tarefa.java
@@ -0,0 +1,27 @@
+package operacoesBasicas.list.listaDeTarefa;
+
+public class Tarefa
+{
+ //atributo
+	private String descricao;
+
+	public Tarefa(String descricao)
+	{
+		this.descricao = descricao;
+	}
+
+	public String getDescricao()
+	{
+		return descricao;
+	}
+
+	@Override
+	public String toString()
+	{
+		return descricao;
+	}
+	
+	
+	
+	
+}
